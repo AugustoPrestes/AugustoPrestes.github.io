d0a69f215c39e969a5e5b6f50877d9275630036a
Augusto <fenkzebra147@gmail.com>
2022-03-01 17:41:26 -0300

    Front-End Finalizado

--- src/script.js
+++ src/script.js
@@ -1,15 +1,64 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(() => {
+    $(window).scroll(() => {
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
+    // script para scroll-up
+    $('.scroll-up-btn').click(() => {
+        $('html').animate({scrollTop: 0});
+    });
+
+
     // Script para alterar menu/navbar 
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(() => {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // Script de animacao
+
+    var typed = new Typed(".habilidades", {
+        strings: ["Desenvolvedor FullStack", "Analista de Dados", "Programador Python"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed = new Typed(".habilidades-2", {
+        strings: ["Desenvolvedor FullStack", "Analista de Dados", "Programador Python"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 200,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
     })
 })
